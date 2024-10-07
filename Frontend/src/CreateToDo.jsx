638fda4b30ad6b22a5da26b356142341bb18e8b4
SnehaBichunde2003 <bichkundesneha@gmail.com>
2024-10-07 23:29:15 +0530

    added frontend

---
+++ Frontend/src/CreateToDo.jsx
@@ -0,0 +1,7 @@
+export function CreateToDo(){
+    return <div>
+        <input type="text" placeholder="title" />
+        <input type="text" placeholder="deccription" />
+        <button>Add todo</button>
+    </div>
+}
